6db5ab9db116e4a229627ffde4cf6b18116fdb7a
magvan temuulen <magvan.temuulen@gmail.com>
2022-08-29 17:58:14 +0200

    association

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,5 @@
 class Plant < ApplicationRecord
   belongs_to :category
+  has_many :tasks
+  has_many :user_plants
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :user_plants
+  has_many :messages
+  has_many :personal_tasks, through: :user_plants
 end
