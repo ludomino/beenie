fdcf5474a53477186a4288f70cd5eb09bf889168
ludovic guadagnin <guadagnin.ludovic@gmail.com>
2022-09-01 13:07:34 +0200

    test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @messages = @category.messages.order(created_at: :DESC)
     # englobe message.new dans un if :
-    # verifier si category_id est inclus dans une des plantes du user ( method .pluck(:nom_de_la_table_voulue) )
+    # verifier si category_id est inclus dans une des plantes du user ( method .pluck(:nom_de_la_table_voulue) w )
     @message = Message.new
   end
 end
