83d12c5ef7180b7f92d26b8c1206b58e1bf766bd
ludovic guadagnin <guadagnin.ludovic@gmail.com>
2022-09-01 13:00:28 +0200

    test maj seed + chat fonctionnelle

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,8 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @messages = @category.messages.order(created_at: :DESC)
+    # englobe message.new dans un if :
+    # verifier si category_id est inclus dans une des plantes du user ( method .pluck(:nom_de_la_table_voulue) )
     @message = Message.new
   end
 end
