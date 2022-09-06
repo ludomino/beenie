7f51d965dd375046135a5b05c6fd3d0bc62e73a5
ludovic guadagnin <guadagnin.ludovic@gmail.com>
2022-09-06 17:17:12 +0200

    chat okkk

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,14 @@ class MessagesController < ApplicationController
     if @message.save
       CategoryChannel.broadcast_to(
         @category,
-        render_to_string(partial: "message", locals: { message: @message })
+        {
+          html: render_to_string(partial: "message", locals: { message: @message }), # broadcast the current_user's id so it is possible to verify in the
+          # stimulus controller if it corresponds or not with
+          # the client's current_user
+          user_id: @message.user.id,
+          username: @message.user.nickname,
+          userimg: @message.user.photo.key
+        }
       )
       head :ok
     else
