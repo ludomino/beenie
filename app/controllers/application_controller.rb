4cbf7fd5cf367b5c660f14f4b6f365ff454b3bb7
Jordanna Pensieri <j.pensieri@gmail.com>
2022-09-07 11:59:36 +0200

    meta added, arrow modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname address photo])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[nickname address photo])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
