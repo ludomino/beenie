6c7f872f7104486e632f2eee603e022992a25510
Loys Linyekula <loys175@gmail.com>
2022-09-07 08:54:25 +0200

    derniere version de l'update du css + logpage

---
+++ app/controllers/logpages_controller.rb
@@ -0,0 +1,2 @@
+class LogpagesController < ApplicationController
+end
---
+++ test/controllers/logpages_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LogpagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
